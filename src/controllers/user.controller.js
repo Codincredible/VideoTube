1fe221e22e6d377c6a0ade2db24b0f8cfbb53dbd
Anurag <anuragkamalinsan@gmail.com>
2026-01-09 02:19:21 +0530

    add error condition while uploading on cloudinary the avatar and cover image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -248,6 +248,10 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
 
+  if (!avatar.url) {
+    throw new ApiError(400, "Error while uploading avatar on cloudinary");
+  }
+
   const user = await User.findByIdAndUpdate(
     req.user._id,
     {
@@ -269,10 +273,14 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   const coverImageLocalPath = req.file?.path;
 
   if (!coverImageLocalPath) {
-    throw new ApiError(400, "Avatar file missing");
+    throw new ApiError(400, "Cover Image file missing");
   }
 
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage || !coverImage.url) {
+    throw new ApiError(400, "Error while uploading cover image on cloudinary");
+  }
 
   const user = await User.findByIdAndUpdate(
     req.user._id,
